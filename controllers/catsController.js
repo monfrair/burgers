7d561216f6479e3c257c02ceb322dec091e8c437
Bill Sorrentino <williamsorrentino@gmail.com>
2017-10-30 17:29:13 -0400

    fixing some errors in code

--- controllers/catsController.js
+++ controllers/catsController.js
@@ -3,7 +3,7 @@ var express = require("express");
 var router = express.Router();
 
 // Import the model (cat.js) to use its database functions.
-var cat = require("../models/burger.js");
+var cat = require("../models/cat.js");
 
 // Create all our routes and set up logic within those routes where required.
 router.get("/", function (req, res) {
