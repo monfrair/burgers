4b77783ac76447e32d92d1f98abdee3edbffa004
Bill Sorrentino <williamsorrentino@gmail.com>
2017-10-30 19:19:02 -0400

    used class example to set up app and now need to change data in order to make it work properly

--- config/connection.js
+++ config/connection.js
@@ -2,10 +2,11 @@
 var mysql = require("mysql");
 
 var connection = mysql.createConnection({
-    port: 3306,
+
     host: "localhost",
+    port: 3306,
     user: "root",
-    password: "",
+    password: "root",
     database: "cat_db"
 });
 
