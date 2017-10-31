897d86c6fee7958aa1f393def5cf126b14170d0b
Bill Sorrentino <williamsorrentino@gmail.com>
2017-10-30 20:36:50 -0400

    wtf

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 
+var app = express();
+
 var PORT = process.env.PORT || 3000;
 
-var app = express();
+
 
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
